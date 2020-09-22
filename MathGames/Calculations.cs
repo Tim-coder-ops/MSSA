a6edf05f9a3fa4b4d7a947f9a69aa593a7f450c0
Tim-coder-ops <timweigel63@gmail.com>
2020-09-21 19:40:50 -0700

    Added Math Game

---
+++ MathGames/Calculations.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathGames
+{
+    class Calculations
+    {
+        static Random ran = new Random();
+
+        static int maxNum = 0;
+        static int minNum = 0;
+
+        static void generateNum()
+        {
+            maxNum = ran.Next(1, 12);
+            minNum = ran.Next(1, 12);
+
+            if(minNum > maxNum)
+            {
+                int temp = maxNum;
+                maxNum = minNum;
+                maxNum = temp;
+            }
+        }
+        public static int Add(int probs)
+        {
+            int score = 0;
+            Console.WriteLine($"You are testing Addition and you have {probs} problems.");
+
+            for(int i =0; i < probs; i++)
+            {
+                generateNum();
+                int answer = maxNum + minNum;
+                Console.Write($"{maxNum} + {minNum} = ");
+                if (int.Parse(Console.ReadLine()) == answer)
+                {
+                    Console.WriteLine("Correct");
+                    score++;
+                }
+                else Console.WriteLine($"Sorry, the correct answer is {answer}.");
+            }
+
+            return score;
+        }
+
+        public static int Subtract(int probs)
+        {
+            int score = 0;
+            Console.WriteLine($"You are testing Subtraction and you have {probs} problems.");
+
+            for (int i = 0; i < probs; i++)
+            {
+                generateNum();
+                int answer = maxNum - minNum;
+                Console.Write($"{maxNum} - {minNum} = ");
+                if (int.Parse(Console.ReadLine()) == answer)
+                {
+                    Console.WriteLine("Correct");
+                    score++;
+                }
+                else Console.WriteLine($"Sorry, the correct answer is {answer}.");
+            }
+
+            return score;
+
+        }
+       
+        public static int Multiply(int probs)
+        {
+            int score = 0;
+            Console.WriteLine($"You are testing Multiplication and you have {probs} problems.");
+
+            for (int i = 0; i < probs; i++)
+            {
+                generateNum();
+                int answer = maxNum * minNum;
+                Console.Write($"{maxNum} * {minNum} = ");
+                if (int.Parse(Console.ReadLine()) == answer)
+                {
+                    Console.WriteLine("Correct");
+                    score++;
+                }
+                else Console.WriteLine($"Sorry, the correct answer is {answer}.");
+            }
+
+            return score;
+
+        }
+       
+        public static int Divide(int probs)
+        {
+            int score = 0;
+            Console.WriteLine($"You are testing Division and you have {probs} problems.");
+
+            for (int i = 0; i < probs; i++)
+            {
+                generateNum();
+                double num1 = maxNum;
+                double num2 = minNum;
+                double answer = Math.Round( num1 / num2, 2);
+                Console.Write($"{maxNum} / {minNum} = ");
+                double userInput = double.Parse(Console.ReadLine());
+                if ( userInput == answer || userInput == answer - 0.01 )
+                {
+                    Console.WriteLine("Correct");
+                    score++;
+                }
+                else Console.WriteLine($"Sorry, the correct answer is {answer}.");
+            }
+
+            return score;
+
+        }
+
+    }
+}
---
+++ MathGames/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Transactions;
+
+namespace MathGames
+{
+    class Program
+    {
+        static int selection = 0;
+        static int numOfProblems = 0;
+        static int numOfCorrect = 0;
+
+        static void initialize(int selection)
+        {
+            try
+            {
+                Console.Write("Enter number of problems between 1 and 12:");
+                numOfProblems = int.Parse(Console.ReadLine());
+                if (numOfProblems < 1 || numOfProblems > 12)
+                {
+                    throw new FormatException();
+                }
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Please enter a valid selection");
+                initialize(selection);
+            }
+
+            if (selection == 1) numOfCorrect = Calculations.Add(numOfProblems);
+            else if (selection == 2) numOfCorrect = Calculations.Subtract(numOfProblems);
+            else if (selection == 3) numOfCorrect = Calculations.Multiply(numOfProblems);
+            else if (selection == 4) numOfCorrect = Calculations.Divide(numOfProblems);
+        }
+
+        static int welcome()
+        {
+           
+
+            Console.WriteLine("\nWelcome to Math Games");
+            Console.Write("\tTo add, enter 1,\n" +
+                "\tTo subtract, enter 2,\n" +
+                "\tTo multiply, enter 3,\n" +
+                "\tTo divide, enter 4.\n" +
+                "Choose your problem type:");
+            try
+            {
+                selection = int.Parse(Console.ReadLine());
+                if (selection < 1 || selection > 4)
+                {
+                    Console.WriteLine("Please enter a valid selection");
+                    welcome();
+                }
+
+            }
+
+            catch
+            {
+                Console.WriteLine("Please enter only integers");
+                welcome();
+            }
+
+            return selection;
+        }
+
+        static string report(int correct, int problems)
+        {
+            double numCorrect = correct;
+            double outOf = problems;
+            double percentage = Math.Round((numCorrect / outOf) *100,0);
+            return$" You got {correct} out of {problems} correct and your grade is {percentage}%";
+        }
+        static void Main(string[] args)
+        {
+            
+            initialize(welcome());
+            Console.WriteLine(report(numOfCorrect,numOfProblems));
+        }
+    }
+}
