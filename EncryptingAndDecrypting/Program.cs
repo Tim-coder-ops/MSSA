63a026afaa09d53e8abfeb5f8e5acc9bb71a0842
Tim-coder-ops <timweigel63@gmail.com>
2020-09-28 19:54:44 -0700

    Added Encryption and Decryption

---
+++ EncryptingAndDecrypting/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace EncryptingAndDecrypting
+{
+    class Program
+    {
+        static void initialize()
+        {
+            string plainText = "";
+            string singleKey;
+            string multiKey = "";
+            int[] contKey;
+
+            Console.Write("Enter plain text:");
+            plainText = Console.ReadLine();
+            Console.Write("Enter your single key as an alpha character:");
+            singleKey = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Enter your multi key as alpha characters:");
+            multiKey = Console.ReadLine();
+            contKey = Util.GetContKey(multiKey, plainText);
+
+            Console.WriteLine($"\nYou entered [{plainText}] as plain text");
+            Console.WriteLine($"you entered [{singleKey}] as your single key");
+            Console.WriteLine($"You entered [{multiKey}] as your multi key\n");
+
+            string singEncrypted = Util.SimpleEncryption(singleKey, plainText);
+            string multiEncrypted = Util.MultiEncryption(multiKey, plainText);
+            string contEncrypted = Util.ContEncryption(contKey, plainText);
+            Console.WriteLine($"Encrypted message with single key is[{singEncrypted}]");
+            Console.WriteLine($"Encrypted message with multi key is[{multiEncrypted}]");
+            Console.WriteLine($"Encrypted message with continuous key is[{contEncrypted}]\n");
+
+            Console.WriteLine($"Decrypted message with single key is[{Util.SimpleDecryption(singleKey, singEncrypted)}]");
+            Console.WriteLine($"Decrypted message with multi key is[{Util.MultiDecryption(multiKey, multiEncrypted)}]");
+            Console.WriteLine($"Decrypted message with continuous key is[{Util.ContDecryption(contKey, contEncrypted)}]");
+
+        }
+        static void Main(string[] args)
+        {
+            initialize();
+        }
+    }
+}
---
+++ EncryptingAndDecrypting/Util.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncryptingAndDecrypting
+{
+    class Util
+    {
+        public static string clean(string word)
+        {
+            word = word.ToUpper();
+            string cleanedWord = "";
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                int temp = word[i];
+
+                if (temp > 64 && temp < 91)
+                {
+                    cleanedWord += (char)temp;
+                }
+            }
+
+            return cleanedWord;
+        }
+        public static string SimpleEncryption(string key, string word)
+        {
+            key = clean(key);
+            word = clean(word);
+            int numKey = (int)key[0] - 64;
+            string result = "";
+
+
+            for(int i = 0; i < word.Length; i++)
+            {
+                    result += (Char)(word[i] + numKey);
+            }
+
+            return result;
+        } 
+
+        public static string SimpleDecryption(string key, string word)
+        {
+            key = clean(key);
+            int numKey = (int)key[0] - 64;
+            string result = "";
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                result += (Char)((int)word[i] - numKey);
+            }
+
+            return result;
+        }
+
+        public static string MultiDecryption(string key, string word)
+        {
+            string result = "";
+            key = clean(key);
+            word = clean(word);
+            int[] numKey = new int[key.Length];
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                numKey[i] = (int)key[i] - 64;
+            }
+
+            int index = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (index == numKey.Length) index = 0;
+                result += (Char)((int)word[i] - numKey[index]);
+                index++;
+            }
+
+            return result;
+        }
+
+        public static string MultiEncryption(string key, string word)
+        {
+            string result = "";
+            key = clean(key);
+            word = clean(word);
+            int[] numKey = new int[key.Length];
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                numKey[i] = (int)key[i] - 64;
+            }
+
+            int index = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                    if (index == numKey.Length) index = 0;
+
+                    if((int)word[i] + numKey[index] > 90)
+                    {
+                    result += (Char)((int)word[i] + numKey[index] -26);
+                    }
+
+                    else result += (Char)((int)word[i] + numKey[index]);
+                    index++;
+            }
+
+            return result;
+        }
+
+        public static int[] GetContKey(string key, string word)
+        {
+            key = clean(key);
+            word = clean(word);
+            int[] numKey = new int[word.Length];
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                numKey[i] = (int)key[i] - 64;
+            }
+
+            for (int i = key.Length; i < word.Length; i++)
+            {
+                numKey[i] = (int)word[i - key.Length] - 64;
+            }
+
+            return numKey;
+        }
+        public static string ContEncryption(int[] numKey, string word)
+        {
+            string result = "";
+            word = clean(word);
+            
+
+            int index = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (index == numKey.Length) index = 0;
+
+                if ((int)word[i] + numKey[index] > 90)
+                {
+                    result += (Char)((int)word[i] + numKey[index] - 26);
+                }
+
+                else result += (Char)((int)word[i] + numKey[index]);
+                index++;
+            }
+
+
+            return result;
+        }
+
+        public static string ContDecryption(int[] numKey, string word)
+        {
+            string result = "";
+
+            for (int i = 0; i < word.Length; i++)
+            { 
+                if((int)word[i] - numKey[i] < 65) result += (Char)((int)word[i] - numKey[i] + 26);
+                else result += (Char)((int)word[i] - numKey[i]);
+            }
+
+            return result;
+        }
+    }
+}
