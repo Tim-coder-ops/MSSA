999e788527374283eaaf21ef82b10c016b22c352
Tim-coder-ops <timweigel63@gmail.com>
2020-10-22 22:16:53 -0700

    added filestream to CSV

--- EX7ACSV/Program.cs
+++ EX7ACSV/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace EX7ACSV
 {
@@ -10,7 +12,7 @@ namespace EX7ACSV
         {
             List<string> result = new List<string>();
             string[] words = input.Split("\"");
-            string temp = "";
+            StringBuilder temp = new StringBuilder();
 
             for (int i = 1; i < words.Length; i++)
             {
@@ -22,23 +24,63 @@ namespace EX7ACSV
 
                     if (j > 0 && word[j] == ',' && word[j - 1] == 'N')
                     {
-                        result.Add(temp);
-                        temp = "";
+                        result.Add(temp.ToString());
+                        temp.Clear();
                         j++;
                     }
 
                     if (j == 0 && word[j] == ',' || j == word.Length - 1 && word[j] == ',') temp =temp;
-                    else temp += word[j];
+                    else temp.Append(word[j]);
 
                     
                 }
 
-                if(temp != "" )result.Add(temp);
-                temp = "";
+                if(temp.Length != 0 )result.Add(temp.ToString());
+                temp.Clear();
             }
 
             return result;
 
+        }
+
+        public static void ReadFile()
+        {
+            List<string> lines = new List<string>();
+            string line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\Users\\Timwe\\OneDrive\\Documents\\MSSA\\MSSA\\EX7ACSV\\pres-test.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                        Console.WriteLine($"Current input is {line}");
+                        List<string> output = getCSV(line);
+                        int len = output.Count;
+                        Console.WriteLine($"This line has {len-2} fields. They are:");
+                        foreach (string s1 in output)
+                            Console.WriteLine(s1);
+                 
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+            
         }
         static void Main(string[] args)
         {
@@ -47,6 +89,9 @@ namespace EX7ACSV
             * parse CSV into separate fields and
             * ignore commas within quoted string
             * ***********************/
+
+
+
             Console.WriteLine("Reading CSV with embedded commas");
             List<string> myList = new List<string>();
             string input1 = "\"a,b\",c";
@@ -67,6 +112,8 @@ namespace EX7ACSV
                 foreach (string s1 in output)
                     Console.WriteLine(s1);
             }
+
+            ReadFile();
         }
     }
 }
